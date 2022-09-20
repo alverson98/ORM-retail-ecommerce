fa604b7a6cf408624b9b3d31e8c62eea58b2ecbb
Hannah Alverson <alverson98@yahoo.com>
2022-09-20 17:03:32 -0600

    made each route an async function and added try/catch layout for each route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,48 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
