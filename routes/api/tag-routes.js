1b06f0af4520862696ea048d91ae09cd6c066f6e
Hannah Alverson <alverson98@yahoo.com>
2022-09-20 17:00:35 -0600

    added delete route to delete one tag based on id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -66,12 +66,20 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+  // delete one tag by its `id` value
   try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.json(deletedTag);
+    console.log("Tag was deleted!");
   } catch (err) {
     console.log(err);
   }
-  // delete on tag by its `id` value
 });
 
 module.exports = router;
