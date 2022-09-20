d16f7f0d505633c49f5dd902801c495427f775f2
Hannah Alverson <alverson98@yahoo.com>
2022-09-20 16:31:17 -0600

    added get route to get all tags including its product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,12 +4,16 @@ const { Tag, Product, ProductTag } = require("../../models");
 // The `/api/tags` endpoint
 
 router.get("/", (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
   try {
+    const allTagData = Tag.findAll({
+      include: [{ model: Product }, { model: Product, through: ProductTag }],
+    });
+    res.json(allTagData);
   } catch (err) {
     console.log(err);
   }
-  // find all tags
-  // be sure to include its associated Product data
 });
 
 router.get("/:id", (req, res) => {
